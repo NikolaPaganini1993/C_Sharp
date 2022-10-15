1a42a07621e199d9d6cd73280ff19f22f156bdca
«Nikola» <plotnikov@allremax.ru>
2022-10-15 18:19:53 +0800

    Added Lesson_6_Homework

---
+++ Lesson_6/HW_6_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int HowManyPositive(int[] array)
+{
+    int positive = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int temporary = 0;
+        temporary = Convert.ToInt32(Console.ReadLine());
+        array[i] = temporary;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) positive++;
+    }
+    return positive;
+}
+Console.Write("Сколько цифр вы собираетесь написать: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int positive = HowManyPositive(array);
+Console.WriteLine($"Количество положительных чисел: {positive}");
